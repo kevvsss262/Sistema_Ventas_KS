8ec61ccdf9adee92cad20c5eec2db671c2150d98
kevin <kevincascante262@gmail.com>
2026-04-22 21:31:37 -0500

    feature: creado y finalizado interfaz categoria, creado sin finalizar categoriaDAO

--- src/entidades/Categoria.java
+++ src/entidades/Categoria.java
@@ -49,6 +49,11 @@ public class Categoria {
     public void setActivo(boolean activo) {
         this.activo = activo;
     }
+
+    @Override
+    public String toString() {
+        return "Categoria{" + "id=" + id + ", nombre=" + nombre + ", descripcion=" + descripcion + ", activo=" + activo + '}';
+    }
     
     
     
