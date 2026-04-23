9cc09dd96920e017c58dec5d8c5b073f83dba49c
kevin <kevincascante262@gmail.com>
2026-04-22 21:15:14 -0500

    feat: Creamos interfaz categoria y DAO categoria

---
+++ src/datos/CategoriaDAO.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package datos;
+
+import java.sql.PreparedStatement;
+import database.conexion;
+import datos.interfaces.CrudSimpleInterface;
+import entidades.Categoria;
+import java.util.List;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Kevin
+ */
+public class CategoriaDAO implements CrudSimpleInterface<Categoria>{
+    
+    private final conexion CON;
+    private PreparedStatement ps;
+    private ResultSet rs;
+    private boolean resp;
+    
+    public CategoriaDAO(){
+        CON = conexion.getInstancia();
+    }
+
+    @Override
+    public List<Categoria> listar(String texto) {
+        List<Categoria> registros = new ArrayList();
+        try {
+            ps=CON.conectar().prepareStatement("SELECT * FROM categoria WHERE nombre LIKE ?");
+            ps.setString(1, "%+" + texto + "%");
+            rs=ps.executeQuery();
+            while (rs.next()) {                
+                registros.add(new Categoria(rs.getInt(1), rs.getString(2), rs.getNString(3), rs.getBoolean(4)));
+            }
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, e.getMessage());
+        }
+    }
+
+    @Override
+    public boolean insertar(Categoria obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean actualizar(Categoria obj) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean desactivar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean activar(int id) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public int total() {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    @Override
+    public boolean existe(String texto) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
